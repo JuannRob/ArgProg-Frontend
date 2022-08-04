5dbbe1e525bd13670316defbd8bd3a0b7ff90d63
Juan Robledo <juannrob@gmail.com>
2022-08-04 02:11:36 -0300

    post test

--- src/app/services/userdata.service.ts
+++ src/app/services/userdata.service.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserdataService {
 
+  URL_BASE:string = `https://myportfolio-argprog.herokuapp.com`;
+
   constructor(private http:HttpClient) { }
 
+
   getUserData():Observable<any>{
-    return this.http.get('https://myportfolio-argprog.herokuapp.com/personas/1')
+    return this.http.get(`${this.URL_BASE}/personas/1/`)
   }
 
   getUserUbication():Observable<any>{
-    return this.http.get('https://myportfolio-argprog.herokuapp.com/personas/1/ubicacion')
+    return this.http.get(`${this.URL_BASE}/personas/1/ubicacion`)
   }
+
+  saveUser(user:any){
+    this.http.post(`${this.URL_BASE}/personas`, user).subscribe((result) => {
+      console.warn('result: ', result)
+    })
+  }
+
+  
 }
